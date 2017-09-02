fb0ec7984f9794ad26049fbf938ffbd87245fd66
Carlota Bindner <cev.bindner@gmail.com>
2017-09-02 02:46:09 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,8 @@ class MP3Importer
   end
 
   def import
-    self.files.each { |file| Song.new_by_filename(file) }
+    files = self.files
+    files.each { |file| Song.new_by_filename(file) }
   end
 
 end
