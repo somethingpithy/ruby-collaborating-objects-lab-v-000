5bd5cca35dbbc4d4db172e2550e1416078172a2d
Carlota Bindner <cev.bindner@gmail.com>
2017-09-02 02:52:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,3 @@
-
 class Song
   attr_accessor :name, :artist
 
@@ -7,8 +6,10 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
+    file = filename.split(" - ")
+    artist = file[0]
+    song_name = file[1]
+    new_song = self.new(song_name)
     new_song.artist_name = artist
     new_song
   end
@@ -17,4 +18,5 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+
 end
