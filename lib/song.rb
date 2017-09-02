0438ce3da6a70847d145a4cb29579194897e5a63
Carlota Bindner <cev.bindner@gmail.com>
2017-09-02 02:39:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,11 +10,11 @@ class Song
     artist = file[0]
     song = file[1]
     song = self.new(song)
-    song.artist = artist
+    song.artist_name = artist
     song
   end
 
-  def artist=(name)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
